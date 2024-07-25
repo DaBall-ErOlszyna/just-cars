84358fcbab97d4c77ee16da2480b778f15ca7a38
DaBall-ErOlszyna <daballerolszyna@gmail.com>
2024-07-25 20:44:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 async function list_directory(user, repo, directory) {
-  const url = `https://api.github.com/repos/${user}/${repo}/git/trees/master`;
+  const url = `https://api.github.com/repos/${user}/${repo}/git/trees`;
   const list = await fetch(url).then(res => res.json());
   const dir = list.tree.find(node => node.path === directory);
   if (dir) {
@@ -11,7 +11,7 @@ async function list_directory(user, repo, directory) {
 
 (async ()=>{
 
-  const files = list_directory("DaBall-ErOlszyna", "just-cars", "/pictures/");
+  const files = await list_directory("DaBall-ErOlszyna", "just-cars", "/pictures/");
 
   console.log(files);
   
